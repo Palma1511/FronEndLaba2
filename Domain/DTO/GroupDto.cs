825c75e541d8af126b8557a7db18604bb6854069
admin <admin@DESKTOP-9H6DVR1>
2023-12-23 10:59:12 +0300

    Добавьте файлы проекта.

---
+++ Domain/DTO/GroupDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTO
+{
+    public class GroupDto
+    {
+        public Guid Id { get; set; }
+        public string NameGroup { get; set; }
+        public List<StudentDto> Students { get; set; } = new List<StudentDto>();
+    }
+}
---
+++ Domain/Models/Group.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Group
+    {
+        public Guid Id { get; set; }
+        public string NameGroup { get; set; }
+        public List<Student> Students { get; set; } = new List<Student>();
+
+        public void AddStudent(Student student)
+        {
+            Students.Add(student);
+        }
+        public void RemoveAt(int index)
+        {
+            Students.RemoveAt(index);
+        }
+        public int StudentCount { get { return Students.Count; } }
+    }
+}
